8fbec916451f23837a10b0f187291113ee5452da
Ornstein385 <valerfom2022@yandex.ru>
2024-02-17 14:06:04 +0300

    hw1 v1

--- bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
@@ -1,13 +1,18 @@
 package edu.java.bot.configuration;
 
-import jakarta.validation.constraints.NotEmpty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-@Validated
-@ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record ApplicationConfig(
-    @NotEmpty
-    String telegramToken
-) {
+@Component
+@Getter
+public class ApplicationConfig {
+
+    //@NotEmpty
+    @Value("${app.telegramToken}")
+    private String telegramToken;
+
+    //@NotEmpty
+    @Value("${app.telegramName}")
+    private String telegramName;
 }
